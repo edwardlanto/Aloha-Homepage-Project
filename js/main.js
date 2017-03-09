93cc42558ad7db816f68c05b6a900ab0e7da7698
Edward <you@email.com>
2017-03-08 21:00:33 -0800

    added and styled shop cart incrementer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    $('.subscribe-button').click(function() {
-      
+$(document).ready(function () {
+    $('.subscribe-button').click(function () {
+
         var sEmail = $('.email-form').val();
         if ($.trim(sEmail).length == 0) {
             alert('Please enter valid email address');
@@ -17,39 +17,48 @@ $(document).ready(function() {
         }
 
     });
-//subscribe-button click bracket
+    //subscribe-button click bracket
 
-function validateEmail(sEmail) {
-    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    if (filter.test(sEmail)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}// validateEmail function bracket
-  
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  }); //smooth-scroll function bracket
+    function validateEmail(sEmail) {
+        var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        if (filter.test(sEmail)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }// validateEmail function bracket
+
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    }); //smooth-scroll function bracket
 
 
-$('.main-carousel').flickity({
-		cellAlign: 'left',
-		contain: true,
-		percentPosition: true,
-		imagesLoaded: true,
-		autoPlay: true,
-		prevNextButtons: false
-	});//main-carousel bracket
+    $('.main-carousel').flickity({
+        cellAlign: 'left',
+        contain: true,
+        percentPosition: true,
+        imagesLoaded: true,
+        autoPlay: true,
+        prevNextButtons: false
+    });//main-carousel bracket
+
+    document.getElementById("incrementer").setAttribute("readonly", "true");
+    var i = 0;
+    $(".increment-button").click(function () {
+        i++;
+        document.getElementById("incrementer").value = i;
+    })
+
+
 
 });//DOCUMENT.ready bracket
